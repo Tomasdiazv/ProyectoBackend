5b2db29c505bdc4e030bf3df9bc175c22a139818
Tomas Diaz <tomas_diaz@live.cl>
2023-01-23 23:15:37 -0300

    se agregan get, post, put y delet

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
-const { response } = require("express")
-const express =  require("express")
-const { request } = require("http")
- const app = express()
+const express =  require("express");
 
- app.get("/", (request, response) => {
-    response.send("Hola mundo")
- }) 
+const app = express();
 
- app.listen(3005, () => {
+let productos = ['pantalon 1', 'pantalon 2'];
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+   res.json({
+      productos
+  }) 
+});
+
+app.post('/', (req, res) => {
+   const producto = req.body.producto;
+   productos.push(producto);
+   res.status(200).json({
+       msg: 'se creo exitosamente el producto',
+       productos
+   })
+});
+
+app.put('/:index', (req, res) => {
+   const index = req.params.index;
+   const producto = req.body.producto;
+
+   productos[index] = producto;
+   res.json({
+      msg:`producto (${index}) fue actualizado con exito `,
+      productos
+   })
+});
+
+app.delete('/:index', (req, res) => {
+   const index = req.params.index;
+   const productosFiltered = productos.filter((producto, i) => i != index);
+
+   productos = productosFiltered;
+
+   res.json({
+      msg:'se elimino correctamente los productos',
+      productos
+   })
+})
+
+app.listen(3005, () => {
     console.log("servidor activo")
  })
